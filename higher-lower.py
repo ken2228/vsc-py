1a0e36641faf742b1e3e35f1550b1a3fe83afc11
d. Schlogl <danny_sleugel@hotmail.com>
2024-03-27 21:08:54 +0100

    modified readability

--- higher-lower.py
+++ higher-lower.py
@@ -17,78 +17,83 @@ from art_hl import vs
 from hl_gamedata import data 
 from os import system
 
-class subject:
+
+class Subject:
     """class to hold the subject information for easier handling in the program.\n
 Takes a dictionairy with the following keywords: 'name', 'follower_count', 'description', 'country'"""
-    def __init__(self, aDictionary: dict):
-        self.name = aDictionary['name']
-        self.followers = aDictionary['follower_count']
-        self.description = aDictionary['description']
-        self.country = aDictionary['country']
+    def __init__(self, adictionary: dict):
+        self.name = adictionary['name']
+        self.followers = adictionary['follower_count']
+        self.description = adictionary['description']
+        self.country = adictionary['country']
 
-def pickFromData():
+
+def pick_from_data():
     """Will return a random data item from the list as a dictionary"""
     return random.choice(data)
 
-def printsubjectinfo(AorB: str, subject: subject):
-    """Will print the 'AorB' item and the subjects information except follower_count"""
-    if str(AorB).lower() == 'a':
-        printstr = "Compare A: "
-    if str(AorB).lower() == 'b':
-        printstr = "Against B: "
-    print(f"{printstr}{subject.name}, a {subject.description}, from {subject.country}.")
 
-def comparesubjectfollowers(subA: subject, subB: subject):
+def print_subject_info(a_or_b: str, subject: Subject):
+    """Will print the 'AorB' item and the subjects information except follower_count"""
+    print_string = ""
+    if str(a_or_b).lower() == 'a':
+        print_string = "Compare A: "
+    if str(a_or_b).lower() == 'b':
+        print_string = "Against B: "
+    print(f"{print_string}{subject.name}, a {subject.description}, from {subject.country}.")
+
+
+def compare_subject_followers(sub_a: Subject, sub_b: Subject):
     """returns wich subject has more followers, returns 0 when subA has more, returns 1 when subB has more
     and returns -1 when both failed"""
-    if subA.followers > subB.followers:
+    if sub_a.followers > sub_b.followers:
         return 0
-    elif subA.followers < subB.followers:
+    elif sub_a.followers < sub_b.followers:
         return 1
     else:
         return -1
 
-def procesUserInput():
+
+def process_user_input():
     """Asks the user for an input A or B, if the user gives a wrong input 'errors' and asks again"""
-    userinput = input("Who has more followers? Type 'A' or 'B': ")
-    if userinput.upper() == 'A':
+    user_input = input("Who has more followers? Type 'A' or 'B': ")
+    if user_input.upper() == 'A':
         return 0
-    if userinput.upper() == 'B':
+    if user_input.upper() == 'B':
         return 1
     else:
         print("Invalid input detected try again!")
-        return procesUserInput()
-
+        return process_user_input()
 
 
 def new_game():
-    system("CLS")
+    system("cls")
     print(logo)
 
-    keepplaying = True
+    keep_playing = True
     score = 0
-    subjectA = subject(pickFromData())
-    while keepplaying == True:
+    subject_a = Subject(pick_from_data())
+    while keep_playing:
 
-        subjectB = subject(pickFromData())
+        subject_b = Subject(pick_from_data())
         
         print(f"You current score is: {score}.")
-        printsubjectinfo('A', subjectA)
+        print_subject_info('A', subject_a)
         print(vs)
-        printsubjectinfo('B', subjectB)
+        print_subject_info('B', subject_b)
 
-        if comparesubjectfollowers(subjectA,subjectB) == procesUserInput():
-            system("CLS")
+        if compare_subject_followers(subject_a, subject_b) == process_user_input():
+            system("cls")
             print(logo)
 
             print(f"Correct!")
             score += 1
-            subjectA = subjectB
+            subject_a = subject_b
         else:
             print("Too bad!")
             print(f"But your final score is: {score}!")
-            keepplaying = False
-        print(f"A has {subjectA.followers} followers, while B has {subjectB.followers}.")
+            keep_playing = False
+        print(f"A has {subject_a.followers} followers, while B has {subject_b.followers}.")
 
     if str(input("Would you like to play another game? 'Y' or 'N'. ")).lower() == 'y':
         new_game()
